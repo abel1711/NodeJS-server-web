82bccb69b7c36437a44792bb803a396dd447f92d
Abel Aron Amieva <abelamieva@gmail.com>
2024-01-30 23:56:37 -0300

    implementacion de prima y postgres con docker completa, dtos creados

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { prisma } from "../../data/postgres";
+import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
 
 const todos = [
     { id: 1, todo: 'buy milk', createdAt: new Date() },
@@ -10,24 +12,54 @@ export class TodosController {
 
     constructor() { }
 
-    public getTodos = (req: Request, res: Response) => {
+    public getTodos = async (req: Request, res: Response) => {
+        const todos = await prisma.todo.findMany();
         res.json(todos);
     };
-    public getTodosById = (req: Request, res: Response) => {
+
+    public getTodosById = async (req: Request, res: Response) => {
         const id = +req.params.id;
-        if(isNaN(id)) return res.status(400).json({error: 'ID is not a number, bad request'})
-        const todo = todos.find(todo => todo.id == id);
+        if (isNaN(id)) return res.status(400).json({ error: 'ID is not a number, bad request' });
+
+        const todo = await prisma.todo.findFirst({
+            where: { id }
+        });
 
         (todo)
             ? res.status(200).json(todo)
             : res.status(404).json({ error: `Todo with id ${id} not found` });
     };
 
-    public createTodo = (req: Request, res: Response)=>{
-        const todo = req.body;
-        console.log(todo)
-        res.json(todo)
+    public createTodo = async (req: Request, res: Response) => {
+        const [error, createTodoDto] = CreateTodoDto.create(req.body);
+        if (error) return res.status(400).json({ error });
 
+        const newTodo = await prisma.todo.create({
+            data: createTodoDto!
+        });
+
+        res.json(newTodo);
+
+    }
+
+    public updateTodo = async (req: Request, res: Response) => {
+        const id = +req.params.id;
+        const [error, updateTodoDto] = UpdateTodoDto.create({ ...req.body, id });
+
+        if (error) return res.status(400).json({ error });
+
+        const todo = await prisma.todo.findFirst({
+            where: { id }
+        });
+
+        if (!todo) return res.status(404).json({ error: `Todo with id ${id} not found ` });
+
+        const updatedTodo = await prisma.todo.update({
+            where: { id },
+            data: updateTodoDto!.values
+        });
+
+        res.status(200).json(updatedTodo);
     }
 
 
--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -9,9 +9,10 @@ export class TodoRoutes {
         const router = Router();
         const todoController = new TodosController();
 
-        router.get('/', todoController.getTodos );
-        router.get('/:id', todoController.getTodosById );
-        router.post('/', todoController.createTodo );
+        router.get('/', todoController.getTodos);
+        router.get('/:id', todoController.getTodosById);
+        router.put('/:id', todoController.updateTodo);
+        router.post('/', todoController.createTodo);
 
         return router;
     }
